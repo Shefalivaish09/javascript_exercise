47b2a108e4ecf85bc8bbb49161aba2cee9895004
shefalivaish09 <shefalivaish09@mail.com>
2016-02-09 20:33:54 +0530

    js assignment

---
+++ js_assignment/js/agricultureData.js
@@ -0,0 +1,194 @@
+//protect global data to override
+(function(){
+
+//to require the fs module of node
+var fs = require('fs');
+
+//input file
+var inputFile = 'inputFile/Production-Department_of_Agriculture_and_Cooperation_1.csv';
+var result = [];
+var dataLength;
+var headers;
+
+//read the csv file
+fs.readFile(inputFile,"utf-8",function(err,data){
+  if(err)
+  {
+    return console.log(err);
+  }
+
+  //perform the manupilation on the read data to push data in array of object
+  var lines=data.split("\n");
+      dataLength= lines.length;
+      headers=lines[0].split(",");
+  for(var i=1;i<dataLength-1;i++){
+  var obj = {};
+  var currentline=splitCSV(lines[i]);//.split(",");
+  for(var j=0;j<headers.length;j++){
+        if(headers[j].substr(0,3)===" 3-")
+        {
+          headers[j]="yr_"+headers[j].substr(3,4);
+        }
+		     obj[headers[j]] = currentline[j];
+   }
+  result.push(obj);
+  }
+
+//call function for first requirement
+ var resultOilseed=oilseedVsProduction();
+ dataToJson('outputfile/oilseedVsProduction.json',resultOilseed);
+
+//call function for second requirement
+var resultFoodgrain=foodgrainVsProduction();;
+dataToJson('outputfile/foodgrainVsProduction.json',resultFoodgrain);
+
+//call function for third requirement
+ var resultCropAggr=commCropAggrValueVsYear();
+ dataToJson('outputfile/commCropAggrValueVsYear.json',resultCropAggr);
+
+ //call function for fourth requirement
+var resultRiceProd = riceProduction();
+dataToJson('outputfile/riceProductionStateValueVsYear.json',resultRiceProd);
+
+});
+
+//function to split data in csv file
+function splitCSV(text){
+  var repl = text.replace(", "," ");
+  return repl.trim().split(",");
+};
+
+//filter the data based on the first requirement
+var oilseedVsProduction=(function(){
+               var oilseed_crop =[];
+              for(var k=0;k < dataLength-2 ;k++){
+              var index= result[k].Particulars.indexOf("Agricultural Production Oilseeds");
+              if(index > -1 && (result[k].yr_2013 !=="NA"))
+                  {
+                   if((result[k].Particulars.indexOf("Kharif"))>-1 || (result[k].Particulars.indexOf("Rabi"))>-1)
+                   {
+                     if((result[k].Particulars.replace("Agricultural Production Oilseeds ","")!=="Kharif") && (result[k].Particulars.replace("Agricultural Production Oilseeds ","")!=="Rabi"))
+                     {
+                        var oilseed = {};
+                        oilseed.x = result[k].Particulars.replace("Agricultural Production Oilseeds ","");
+                        oilseed.y =parseFloat(result[k].yr_2013);
+                        oilseed_crop.push(oilseed);
+                     }
+                   }
+                  }
+              }
+
+              //sorting data in descending order
+              oilseed_crop.sort(function(a, b) { return b.y - a.y; });
+
+              //return json stringify data
+              return JSON.stringify(oilseed_crop);
+      });
+
+//filter the data based on the second requirement
+var foodgrainVsProduction=(function(){
+          var foodgrain_crop =[];
+          for(var k=0;k < dataLength-2 ;k++){
+          var index= result[k].Particulars.indexOf("Agricultural Production Foodgrains");
+          if(index>-1 && (result[k].yr_2013 !=="NA"))
+              {
+               if(
+                 (result[k].Particulars.indexOf("Area"))===-1 && (result[k].Particulars.indexOf("Volume"))===-1 && (result[k].Particulars.indexOf("Yield"))===-1 )
+               {
+                 if((result[k].Particulars.indexOf("Kharif"))>-1 || (result[k].Particulars.indexOf("Rabi"))>-1)
+                 {
+                 if((result[k].Particulars.replace("Agricultural Production Foodgrains ","")!=="Kharif") && (result[k].Particulars.replace("Agricultural Production Foodgrains ","")!=="Rabi") && (result[k].Particulars.replace("Agricultural Production Foodgrains ","")!=="Production Foodgrains Coarse Cereals Rice Kharif"))
+                 {
+                    var foodgrain = {};
+                    foodgrain.x = result[k].Particulars.replace("Agricultural Production Foodgrains ","");
+                    foodgrain.y =parseFloat(result[k].yr_2013);
+                    foodgrain_crop.push(foodgrain);
+                 }
+               }
+              }
+            }
+          }
+
+          //sorting data in descending order
+          foodgrain_crop.sort(function(a, b) { return b.y - a.y; });
+
+          //return json stringify data
+          return JSON.stringify(foodgrain_crop);
+  });
+
+//filter the data based on the third requirement
+var commCropAggrValueVsYear=(function(){
+              var aggrComm =[];
+              for(var y=3;y<headers.length;y++){
+              var sum =0;
+              var year = headers[y];
+              for (var k=0;k<dataLength-2;k++){
+                var index =result[k].Particulars.indexOf("Commercial");
+                if(index>-1)
+                {
+                  var rowObj = result[k];
+                  if(rowObj[year] === "NA")
+                  {
+                    rowObj[year]=0;
+                  }
+                  sum = sum + parseFloat(rowObj[year]);
+                }
+             }
+             var aggrComm_data ={};
+              aggrComm_data.x=headers[y];
+              aggrComm_data.y=sum;
+              aggrComm.push(aggrComm_data);
+            }
+
+            //return json stringify data
+            return JSON.stringify(aggrComm);
+  });
+
+//filter the data based on the fourth requirement
+var riceProduction=(function(){
+      var lookUp =[];
+      var southern ={southern_state:['Karnataka','Andhra Pradesh','Tamil Nadu','Kerala','Telangana']};
+      lookUp.push(southern);
+      var riceProd=[];
+      for(var y=3;y<headers.length;y++){
+        var year = headers[y];
+        var value =0;
+        var riceProdData={};
+        riceProdData["year"]=headers[y];
+        for (var k=0;k<dataLength-2;k++) {
+          if((result[k].Particulars.indexOf("Rice Yield"))>-1)
+              {
+                for(var i=0;i<lookUp[0].southern_state.length;i++)
+                {
+                  if(result[k].Particulars.indexOf(lookUp[0].southern_state[i])>-1)
+                  {
+                    var rowObj = result[k];
+                    if(rowObj[year] === "NA")
+                    {
+                      rowObj[year]=0;
+                    }
+                    value = parseFloat(rowObj[year]);
+                    riceProdData[lookUp[0].southern_state[i]]=value;
+                  }
+                }
+              }
+            }
+          riceProd.push(riceProdData);
+      }
+
+      //return json stringify data
+      return JSON.stringify(riceProd);
+});
+
+//code to convert data into json format
+var dataToJson = function(file_name,file_data){
+    fs.writeFile(file_name, file_data, function (err) {
+     if (err) {
+       console.log('There has been an error saving your configuration data.');
+       console.log(err.message);
+       return;
+     }
+     console.log(file_name + ' file saved successfully.');
+     });
+   };
+}());
---
+++ js_assignment/js/codebar.js
@@ -0,0 +1,204 @@
+//js for bar chart
+function myFunction (el,json_file,graph_color){
+		var margin = {top: 40, right: 20, bottom: 200, left: 40},
+					width =900 - margin.left - margin.right,
+					height =600 - margin.top - margin.bottom,
+					x = d3.scale.ordinal().rangeRoundBands([0, width],.4),
+					y = d3.scale.linear().range([height, 0]),
+
+					xAxis = d3.svg.axis()
+					.scale(x)
+					.orient("bottom")
+					.tickSize(2),
+					yAxis = d3.svg.axis()
+					.scale(y)
+					.orient("left")
+					.ticks(15)
+					.tickSize(2)
+					.tickFormat(d3.format(".2s")),
+
+					svg = d3.select(el).append("svg")
+					.attr("width", width + margin.left + margin.right)
+					.attr("height", height + margin.top + margin.bottom)
+					.append("g")
+					.attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+
+		   //read json file
+					d3.json(json_file, function(error, data) {
+					    data.forEach(function(d) {
+								d.x = d.x;//for x axis data
+								d.y = +d.y;//for y axis data
+					    });
+
+							x.domain(data.map(function(d) { return d.x; }));
+							y.domain([0, d3.max(data, function(d) { return d.y; })]);
+
+							//code for tooltip
+							var tip = d3.tip()
+										.attr('class', 'd3-tip')
+										.offset([-10, 0])
+										.html(function(d) {
+											return "<strong>Production:</strong> <span style='color:red'>" + d.y + "</span>";
+										});
+
+
+							svg.append("g")
+									.attr("class", "x axis")
+									.attr("transform", "translate(0," + height + ")")
+									.call(xAxis)
+									.selectAll("text")
+									.style("text-anchor", "end")
+									.attr("dx", "-.8em")
+									.attr("dy", "-.55em")
+									.attr("transform", "rotate(-90)" );
+
+									//call tooltip
+									svg.call(tip);
+
+							svg.append("g")
+									.attr("class", "y axis")
+									.call(yAxis)
+							    .append("text")
+									.attr("transform", "rotate(-90)")
+									.attr("y", 6)
+									.attr("dy", ".71em")
+									.style("text-anchor", "end")
+									.text("Production");
+
+
+
+							svg.selectAll("bar")
+									.data(data)
+						   		.enter().append("rect")
+									.style("fill", graph_color)
+									.attr("x", function(d) { return x(d.x); })
+									.attr("width", x.rangeBand())
+									.attr("y", function(d) { return y(d.y); })
+									.attr("height", function(d) { return height - y(d.y); })
+									.on('mouseover', tip.show)
+      						.on('mouseout', tip.hide);
+
+
+						});
+			};
+
+		//js for stack bar chart
+
+		function stackBar(el,json_file){
+			var margin = {top: 32, right: 20, bottom: 70, left: 40},
+			    width = 900 - margin.left - margin.right,
+			    height = 500 - margin.top - margin.bottom;
+
+			var x = d3.scale.ordinal()
+			    .rangeRoundBands([0, width], .4);
+
+			var y = d3.scale.linear()
+			    .rangeRound([height, 0]);
+
+			var color = d3.scale.ordinal()
+			    .range(["#9999ff", "#3333ff", "#0000b3", "#000066"]);
+
+			var xAxis = d3.svg.axis()
+			    .scale(x)
+			    .orient("bottom")
+					.tickSize(2);
+
+			var yAxis = d3.svg.axis()
+			    .scale(y)
+			    .orient("left")
+					.tickSize(2)
+			    .tickFormat(d3.format(".2s"));
+
+			var svg = d3.select(el).append("svg")
+			    .attr("width", width + margin.left + margin.right )
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			d3.json(json_file, function(error, data) {
+			  if (error) throw error;
+
+			  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
+
+			  data.forEach(function(d) {
+			    var y0 = 0;
+			    d.prod = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+			    d.total = d.prod[d.prod.length - 1].y1;
+			  });
+
+			  // data.sort(function(a, b) { return b.total - a.total; });
+
+			  x.domain(data.map(function(d) { return d.year; }));
+			  y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+
+				//code for tooltip
+				var tip = d3.tip()
+							.attr('class', 'd3-tip')
+							.offset([-10, 0])
+							.html(function(d) {
+								return "<strong>Production:</strong> <span style='color:red'>" + d.y1 + "</span>";
+							});
+
+				svg.append("g")
+						.attr("class", "x axis")
+						.attr("transform", "translate(0," + height + ")")
+						.call(xAxis)
+						.selectAll("text")
+						.style("text-anchor", "end")
+						.attr("dx", "-.8em")
+						.attr("dy", "-.55em")
+						.attr("transform", "rotate(-90)" );
+
+						//call tooltip
+						svg.call(tip);
+
+			  svg.append("g")
+			      .attr("class", "y axis")
+			      .call(yAxis)
+			    .append("text")
+			      .attr("transform", "rotate(-90)")
+			      .attr("y", 6)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "end")
+			      .text("Production");
+
+			  var year = svg.selectAll(".year")
+			      .data(data)
+			    .enter().append("g")
+			      .attr("class", "g")
+			      .attr("transform", function(d) { return "translate(" + x(d.year) + ",0)"; });
+
+			  year.selectAll("rect")
+			      .data(function(d) { return d.prod; })
+			    .enter().append("rect")
+			      .attr("width", x.rangeBand())
+			      .attr("y", function(d) { return y(d.y1); })
+			      .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+			      .style("fill", function(d) { return color(d.name); })
+						.on('mouseover', tip.show)
+						.on('mouseout', tip.hide);
+
+			  var legend = svg.selectAll(".legend")
+			      .data(color.domain().slice().reverse())
+			      .enter().append("g")
+			      .attr("class", "legend")
+			      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+			  legend.append("rect")
+			      .attr("x", width + 10)
+						.attr("y", "-30px")
+			      .attr("width", 18)
+			      .attr("height", 18)
+			      .style("fill", color);
+
+			  legend.append("text")
+			      .attr("x", width + 8)
+			      .attr("y", 9)
+			      .attr("dy", "-30px")
+			      .style("text-anchor", "end")
+			      .text(function(d) { return d; });
+
+			});
+	};
